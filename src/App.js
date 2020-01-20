7d17e6c679c50ecc01bec9f332f3ce29350efec1
WangXiongShi <wangxiongshi@xiaohe.com>
2020-01-20 16:49:14 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assats/images/logo.svg';
+import './assats/css/App.css';
 
 function App() {
   return (
