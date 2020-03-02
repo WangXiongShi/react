77129a0c0f7c09326fbc2ec6e274532e5b451d23
WangXiongShi <wangxiongshi@xiaohe.com>
2020-03-02 14:50:57 +0800

    react插件

---
+++ src/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; //imrc
+class Test extends Component {
+    state = {}
+    render() {
+        return (
+            <div>test</div>
+        );
+    }
+}
+
+export default Test;
